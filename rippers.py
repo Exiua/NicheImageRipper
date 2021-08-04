e658f489c872a72457ec5065f801fb6ad9bb815a
Exiua <master6969697@gmail.com>
2021-08-04 00:59:10 -0500

    tail method helps with video file validation

--- rippers.py
+++ rippers.py
@@ -376,6 +376,29 @@ class ImageRipper():
             return domain
         raise RipperError("Not a support site")
 
+def tail(f, lines=2):
+    total_lines_wanted = lines
+
+    BLOCK_SIZE = 1024
+    f.seek(0, 2)
+    block_end_byte = f.tell()
+    lines_to_go = total_lines_wanted
+    block_number = -1
+    blocks = []
+    while lines_to_go > 0 and block_end_byte > 0:
+        if (block_end_byte - BLOCK_SIZE > 0):
+            f.seek(block_number*BLOCK_SIZE, 2)
+            blocks.append(f.read(BLOCK_SIZE))
+        else:
+            f.seek(0,0)
+            blocks.append(f.read(block_end_byte))
+        lines_found = blocks[-1].count(b'\n')
+        lines_to_go -= lines_found
+        block_end_byte -= BLOCK_SIZE
+        block_number -= 1
+    all_read_text = b''.join(reversed(blocks))
+    return b'\n'.join(all_read_text.splitlines()[-total_lines_wanted:])
+
 def babecentrum_parse(driver: webdriver.Firefox) -> tuple[list[str], int, str]:
     """Read the html for babecentrum.com"""
     #Parses the html of the site
