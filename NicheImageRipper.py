f22429641b99ba34dc86e47fa5b2cf8ddc4098e1
Exiua <master6969697@gmail.com>
2022-09-10 17:10:25 -0500

    Ported over add url field and rip button

--- NicheImageRipper.py
+++ NicheImageRipper.py
@@ -1,53 +1,36 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QLineEdit, QWidget, QFormLayout
-from PyQt5.QtGui import QIntValidator, QDoubleValidator, QFont
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QLineEdit, QWidget, QFormLayout, QPushButton, QHBoxLayout
+from PyQt5.QtGui import QIntValidator, QDoubleValidator, QFont, QIcon
+from PyQt5.QtCore import pyqtSlot
 
 class NicheImageRipper(QWidget):
     def __init__(self, parent: QWidget | None = None):
         super().__init__(parent)
-        e1 = QLineEdit()
-        e1.setValidator(QIntValidator())
-        e1.setMaxLength(4)
-        e1.setAlignment(Qt.AlignRight)
-        e1.setFont(QFont("Arial",20))
+        self.title = "NicheImagerRipper"
+        self.version = "v3.0.0"
+        self.initUI()
 
-        e2 = QLineEdit()
-        e2.setValidator(QDoubleValidator(0.99,99.99,2))
-        e3 = QLineEdit()
-        e3.setInputMask("+99_9999_999999")
+    def initUI(self):
+        first_row = QHBoxLayout()
 
-        e4 = QLineEdit()
-        e4.textChanged.connect(self.textchanged)
+        #region First Row Construction
 
-        e5 = QLineEdit()
-        e5.setEchoMode(QLineEdit.Password)
+        url_field = QLineEdit()
+        url_field.setFont(QFont("Arial"))
+        rip_button = QPushButton("Rip")
+        first_row.addWidget(url_field)
+        first_row.addWidget(rip_button)
 
-        e6 = QLineEdit("Hello PyQt5")
-        e6.setReadOnly(True)
-        e5.editingFinished.connect(self.enterPress)
+        #endregion
 
-        flo = QFormLayout()
-        flo.addRow("integer validator",e1)
-        flo.addRow("Double validator",e2)
-        flo.addRow("Input Mask",e3)
-        flo.addRow("Text changed",e4)
-        flo.addRow("Password",e5)
-        flo.addRow("Read Only",e6)
+        form_layout = QFormLayout()
+        form_layout.addRow("Enter URL:", first_row)
 
-        self.setLayout(flo)
-        self.setWindowTitle("QLineEdit Example")
-
-    def textchanged(self, text):
-        print("Changed: " + text)
-
-    def enterPress(self):
-        print("Enter pressed")
+        self.setLayout(form_layout)
+        self.setWindowTitle(f"{self.title} {self.version}")
 
 if __name__ == "__main__":
-    #rip_gui = RipperGui()
-    #rip_gui.app_gui()
     app = QApplication(sys.argv)
     win = NicheImageRipper()
     win.show()
