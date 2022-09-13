67c23665462c8090b06d6e179114f1760c02f304
Exiua <master6969697@gmail.com>
2022-09-13 10:57:43 -0500

    Created FilenameScheme.py

---
+++ FilenameScheme.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class FilenameScheme(Enum):
+    ORIGINAL = 1
+    HASH = 2
+    CHRONOLOGICAL = 3
+
+
+if __name__ == "__main__":
+    raise Exception("This file is not meant to be ran directly")
