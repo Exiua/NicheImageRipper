f0c221f3a660772c3e3f33932de2636df1d1d066
Exiua <master6969697@gmail.com>
2022-10-15 03:11:41 -0500

    Updated file_compressor.py

--- Util/file_compressor.py
+++ Util/file_compressor.py
@@ -1,7 +1,11 @@
 import os
 
+
 def main():
-    os.system('"C:\Program Files\WinRAR\Rar.exe" a NicheImageRipper.rar NicheRipper.pyw rippers.py')
+    os.system(r'"C:\Program Files\WinRAR\Rar.exe" a NicheImageRipper.rar ./Icons/ NicheRipper.pyw NicheImageRipper.py '
+              r'FilenameScheme.py HtmlParser.py ImageRipper.py requirements.txt RipInfo.py RipperExceptions.py '
+              r'rippers.py StatusSync.py')
+
 
 if __name__ == "__main__":
     main()
