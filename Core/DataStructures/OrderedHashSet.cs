202e3eccd989e0124e7bd80b1af1b2fab9f1b57d
Exiua <master6969697@gmail.com>
2024-07-10 13:41:31 -0500

    translated and updated almost all parsers

---
+++ Core/DataStructures/OrderedHashSet.cs
@@ -0,0 +1,28 @@
+﻿using Core.ExtensionMethods;
+
+namespace Core.DataStructures;
+
+public class OrderedHashSet<T>
+{
+    private readonly List<T> _list = [];
+    private readonly HashSet<T> _set = [];
+    
+    public int Count => _list.Count;
+    
+    public bool Add(T item)
+    {
+        if (!_set.Add(item))
+        {
+            return false;
+        }
+
+        _list.Add(item);
+        return true;
+
+    }
+    
+    public IEnumerable<(int i, T)> Enumerate()
+    {
+        return _list.Enumerate();
+    }
+}
