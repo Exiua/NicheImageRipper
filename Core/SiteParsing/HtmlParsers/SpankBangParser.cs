8565d756722320022086d0f2dab7c32d372e96ac
Exiua <master6969697@gmail.com>
2025-03-21 02:51:18 -0500

    fix url classification in SpankBangParser

--- Core/SiteParsing/HtmlParsers/SpankBangParser.cs
+++ Core/SiteParsing/HtmlParsers/SpankBangParser.cs
@@ -37,47 +37,51 @@ public class SpankBangParser : HtmlParser
         var soup = await Soupify();
         string dirName;
         var images = new List<StringImageLinkWrapper>();
-        if (CurrentUrl.Contains("/playlist/"))
+        var (urlType, id) = GetUrlType();
+        switch (urlType)
         {
-            var id = CurrentUrl.Split("/")[3];
-            dirName = soup.SelectSingleNode("//ul[@class='top profile-top']//em").InnerText + $" ({id})";
-            var videos = soup.SelectSingleNode(playlistXPath)
-                             .SelectNodes("./div")
-                             .Select(div => div.SelectSingleNode(".//a").GetHref())
-                             .Select(src => $"https://spankbang.com{src}");
-            foreach (var video in videos)
+            case UrlType.Unknown:
+                Log.Error("Unknown url type: {Url}", CurrentUrl);
+                throw new RipperException("Unknown url type");
+            case UrlType.Playlist:{
+                dirName = soup.SelectSingleNode("//ul[@class='top profile-top']//em").InnerText + $" ({id})";
+                var videos = soup.SelectSingleNode(playlistXPath)
+                                 .SelectNodes("./div")
+                                 .Select(div => div.SelectSingleNode(".//a").GetHref())
+                                 .Select(src => $"https://spankbang.com{src}");
+                foreach (var video in videos)
+                {
+                    CurrentUrl = video;
+                    var src = await GetVideoUrl();
+                    images.Add(src);
+                }
+                break;
+            }
+            case UrlType.SingleVideo:
             {
-                CurrentUrl = video;
+                dirName = soup.SelectSingleNode("//h1[@class='main_content_title']").InnerText;
                 var src = await GetVideoUrl();
                 images.Add(src);
+                break;
             }
-        }
-        else if (CurrentUrl.Contains("/s/"))
-        {
-            var id = CurrentUrl.Split("/")[3];
-            dirName = soup.SelectSingleNode("//h1[@class='main_content_title']").InnerText + $" ({id})";
-            var videos = soup.SelectSingleNode(playlistXPath)
-                             .SelectNodes("./div")
-                             .Select(div => div.SelectSingleNode(".//a").GetHref())
-                             .Select(src => $"https://spankbang.com{src}");
-            await File.WriteAllTextAsync("test2.html", Driver.PageSource);
-            foreach (var video in videos)
+            case UrlType.Search:
             {
-                CurrentUrl = video;
-                var src = await GetVideoUrl();
-                images.Add(src);
+                dirName = soup.SelectSingleNode("//h1[@class='main_content_title']").InnerText + $" ({id})";
+                var videos = soup.SelectSingleNode(playlistXPath)
+                                 .SelectNodes("./div")
+                                 .Select(div => div.SelectSingleNode(".//a").GetHref())
+                                 .Select(src => $"https://spankbang.com{src}");
+                await File.WriteAllTextAsync("test2.html", Driver.PageSource);
+                foreach (var video in videos)
+                {
+                    CurrentUrl = video;
+                    var src = await GetVideoUrl();
+                    images.Add(src);
+                }
+                break;
             }
-        }
-        else if (CurrentUrl.Contains("/video/"))
-        {
-            dirName = soup.SelectSingleNode("//h1[@class='main_content_title']").InnerText;
-            var src = await GetVideoUrl();
-            images.Add(src);
-        }
-        else
-        {
-            Log.Error("Unknown url type: {Url}", CurrentUrl);
-            throw new RipperException("Unknown url type");
+            default:
+                throw new ArgumentOutOfRangeException();
         }
 
         return new RipInfo(images, dirName, FilenameScheme);
@@ -107,5 +111,32 @@ public class SpankBangParser : HtmlParser
         var soup = await Soupify();
         var src = soup.SelectSingleNode("//video[@id='main_video_player_html5_api']").GetSrc().DecodeUrl();
         return src;
+    }
+
+    private (UrlType, string) GetUrlType()
+    {
+        var id = CurrentUrl.Split("/")[3];
+        if (CurrentUrl.Contains("/playlist/"))
+        {
+            return id.Contains('-') ? (UrlType.SingleVideo, id) : (UrlType.Playlist, id);
+        }
+        if (CurrentUrl.Contains("/video/"))
+        {
+            return (UrlType.SingleVideo, id);
+        }
+        if (CurrentUrl.Contains("/s/"))
+        {
+            return (UrlType.Search, id);
+        }
+        
+        return (UrlType.Unknown, id);
+    }
+    
+    private enum UrlType
+    {
+        Unknown,
+        Playlist,
+        SingleVideo,
+        Search,
     }
 }
