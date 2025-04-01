080be70c9db960f24b41d8ca8fe1525425324ee7
Exiua <master6969697@gmail.com>
2025-03-31 20:25:12 -0500

    introduce GeneralConfig and GuiConfig (extends GeneralConfig)

--- Core/Configuration/GeneralConfig.cs
+++ Core/Configuration/GeneralConfig.cs
@@ -40,7 +40,7 @@ public class GeneralConfig
     protected GeneralConfig(bool _)
     {
         UserAgent =
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36";
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36";
         SavePath = "./Rips/";
         Theme = "Dark";
         FilenameScheme = FilenameScheme.Original;
--- CoreGui/Models/GuiConfig.cs
+++ CoreGui/Models/GuiConfig.cs
@@ -5,6 +5,8 @@ namespace CoreGui.Models;
 
 public class GuiConfig : GeneralConfig
 {
+    public HistoryColumnWidths HistoryColumnWidths { get; set; } = null!;
+    
     [UsedImplicitly]
     public GuiConfig() : base()
     {
@@ -13,6 +15,28 @@ public class GuiConfig : GeneralConfig
 
     protected GuiConfig(bool _) : base(_)
     {
+        HistoryColumnWidths = HistoryColumnWidths.Default;
+    }
+}
+
+public class HistoryColumnWidths
+{
+    public double NameWidth { get; set; }
+    public double UrlWidth { get; set; }
+    public double DateWidth { get; set; }
+    public double CountWidth { get; set; }
+    
+    [UsedImplicitly]
+    public HistoryColumnWidths()
+    {
         
     }
+    
+    public static HistoryColumnWidths Default { get; } = new()
+    {
+        NameWidth = 530,
+        UrlWidth = 530,
+        DateWidth = 150,
+        CountWidth = 100
+    };
 }
