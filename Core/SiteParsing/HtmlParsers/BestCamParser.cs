bc9eeeaa8990d10dbe9b0796dd76b2b814534950
Exiua <master6969697@gmail.com>
2025-10-15 15:00:33 -0500

    add BestCamParser but unable to bypass cloudflare and get playlist url

---
+++ Core/SiteParsing/HtmlParsers/BestCamParser.cs
@@ -0,0 +1,55 @@
+using Common.ExtensionMethods;
+using Core.DataStructures;
+using Core.Enums;
+using Core.ExtensionMethods;
+using Core.SiteParsing.VideoCapturers;
+using OpenQA.Selenium;
+using Serilog;
+using WebDriver = Core.Driver.WebDriver;
+
+namespace Core.SiteParsing.HtmlParsers;
+
+public class BestCamParser : HtmlParser
+{
+    public BestCamParser(WebDriver driver, Dictionary<string, string> requestHeaders,
+                         FilenameScheme filenameScheme = FilenameScheme.Original) : base(driver, requestHeaders,
+        filenameScheme)
+    {
+    }
+
+    /// <summary>
+    ///     Parses the html for bestcam.tv and extracts the relevant information necessary for downloading images from the site
+    /// </summary>
+    /// <returns>A RipInfo object containing the image links and the directory name</returns>
+    public override async Task<RipInfo> Parse()
+    {
+        var (capturer, b) = await ConfigureNetworkCapture<BestCamVideoCapturer>();
+        await using var bidi = b;
+        var soup = await SolveParse();
+        string dirName;
+        var images = new List<StringImageLinkWrapper>();
+        if (CurrentUrl.Contains("/model/"))
+        {
+            dirName = soup.SelectSingleNodeOrThrow("//div[@class='actor-name']/h1").InnerText;
+        }
+        else
+        {
+            dirName = soup.SelectSingleNodeOrThrow("//h1[@class='movie-detail-name']").InnerText;
+            var playButton = Driver.FindElement(By.XPath("//div[@class='play-icon']"));
+            playButton.Click();
+            WaitForPlaylist(capturer, links =>
+            {
+                var url = links[0];
+                var filename = url.Split("/")[4].Split("?")[0].Remove(".m3u8") + ".mp4";
+                var link = new ImageLink(url, FilenameScheme, 0, filename: filename)
+                {
+                    LinkInfo = LinkInfo.M3U8Ffmpeg,
+                    Referer = CurrentUrl
+                };
+                images.Add(link);
+            });
+        }
+
+        return RipInfo.FromUrlList(images, dirName, FilenameScheme);
+    }
+}
