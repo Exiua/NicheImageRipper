03ced1834972f74935d24845d2a89ea6fb2fd0d2
Exiua <master6969697@gmail.com>
2025-10-15 14:36:44 -0500

    add PrivateHomeClipsParser and support for privatehomeclips.com

---
+++ Core/SiteParsing/HtmlParsers/PrivateHomeClipsParser.cs
@@ -0,0 +1,63 @@
+using Common.ExtensionMethods;
+using Core.DataStructures;
+using Core.Enums;
+using Core.ExtensionMethods;
+using Core.SiteParsing.VideoCapturers;
+using WebDriver = Core.Driver.WebDriver;
+
+namespace Core.SiteParsing.HtmlParsers;
+
+public class PrivateHomeClipsParser : HtmlParser
+{
+    public PrivateHomeClipsParser(WebDriver driver, Dictionary<string, string> requestHeaders,
+                                  FilenameScheme filenameScheme = FilenameScheme.Original) : base(driver,
+        requestHeaders, filenameScheme)
+    {
+    }
+
+    /// <summary>
+    ///     Parses the html for privatehomeclips.com and extracts the relevant information necessary for downloading images from the site
+    /// </summary>
+    /// <returns>A RipInfo object containing the image links and the directory name</returns>
+    public override async Task<RipInfo> Parse()
+    {
+        var (capturer, b) = await ConfigureNetworkCapture<PrivateHomeClipsVideoCapturer>();
+        await using var bidi = b;
+        Driver.Refresh();
+        var soup = await Soupify();
+        var dirName = soup.SelectSingleNodeOrThrow("//h1[@class='video-page__title']").ChildNodes[0].InnerText;
+        var images = new List<StringImageLinkWrapper>();
+        var videoUrl = soup.SelectSingleNodeOrThrow("//video").GetSrc();
+        if (videoUrl.StartsWith("blob:"))
+        {
+            WaitForPlaylist(capturer, links =>
+            {
+                var url = links[0];
+                string filename;
+                if (url.Contains(".mp4.m3u8"))
+                {
+                    // Get the last part of the URL and remove the .m3u8 extension
+                    filename = url.Split("/")[^1][..^5];
+                }
+                else
+                {
+                    filename = url.Split("/")[^2];
+                }
+                
+                var link = new ImageLink(url, FilenameScheme, 0, filename: filename)
+                {
+                    LinkInfo = LinkInfo.M3U8YtDlp,
+                    Referer = CurrentUrl
+                };
+                
+                images.Add(link);
+            });
+        }
+        else
+        {
+            images.Add("https://privatehomeclips.com" + videoUrl);
+        }
+
+        return RipInfo.FromUrlList(images, dirName, FilenameScheme);
+    }
+}
---
+++ Core/SiteParsing/VideoCapturers/PrivateHomeClipsVideoCapturer.cs
@@ -0,0 +1,17 @@
+using Core.ExtensionMethods;
+using OpenQA.Selenium.BiDi.Network;
+
+namespace Core.SiteParsing.VideoCapturers;
+
+public class PrivateHomeClipsVideoCapturer : PlaylistCapturer
+{
+    protected override bool ResponseIsInteresting(ResponseCompletedEventArgs e)
+    {
+        return e.Response.Url.Contains(".m3u8");
+    }
+
+    protected override string GetId(string url)
+    {
+        return url.Split("/").Skip(8).Take(2).JoinWith("/");
+    }
+}
