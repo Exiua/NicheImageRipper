f06d49aa11a4749aa4d6828de185ae5544ebcd80
Exiua <master6969697@gmail.com>
2026-04-05 18:28:46 -0500

    update dependencies

--- Gui/App.axaml.cs
+++ Gui/App.axaml.cs
@@ -1,8 +1,10 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 using Gui.ViewModels;
 using Gui.Views;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Gui;
 
@@ -15,9 +17,33 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        // If you use CommunityToolkit, line below is needed to remove Avalonia data validation.
+        // Without this line you will get duplicate validations from both Avalonia and CT
+        BindingPlugins.DataValidators.RemoveAt(0);
+
+        // Register all the services needed for the application to run
+        var collection = new ServiceCollection();
+        collection.AddTransient<MainWindowViewModel>();
+
+        // Creates a ServiceProvider containing services from the provided IServiceCollection
+        var services = collection.BuildServiceProvider();
+
+        var vm = services.GetRequiredService<MainWindowViewModel>();
+        
+        switch (ApplicationLifetime)
         {
-            desktop.MainWindow = new MainWindow();
+            case IClassicDesktopStyleApplicationLifetime desktop:
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = vm,
+                };
+                break;
+            case ISingleViewApplicationLifetime singleViewPlatform:
+                singleViewPlatform.MainView = new MainWindow
+                {
+                    DataContext = vm
+                };
+                break;
         }
 
         base.OnFrameworkInitializationCompleted();
