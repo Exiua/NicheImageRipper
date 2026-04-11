2f6ef87cddf77cee6d7f2e0f9b77dcac66a7acba
Exiua <master6969697@gmail.com>
2026-04-11 15:31:37 -0500

    create IRipperSettings impl for full

---
+++ Gui/Services/Full/FullRipperSettings.cs
@@ -0,0 +1,43 @@
+using Core.Enums;
+using Gui.Models;
+
+namespace Gui.Services.Full;
+
+public class FullRipperSettings : IRipperSettings
+{
+    private static GuiConfig Config => (GuiConfig) Core.Configuration.Config.Instance;
+    
+    public string SavePath
+    {
+        get => Config.SavePath;
+        set => Config.SavePath = value;
+    }
+
+    public FilenameScheme FilenameScheme
+    {
+        get => Config.FilenameScheme;
+        set => Config.FilenameScheme = value;
+    }
+
+    public UnzipProtocol UnzipProtocol
+    {
+        get => Config.UnzipProtocol;
+        set => Config.UnzipProtocol = value;
+    }
+
+    public int MaxRetries
+    {
+        get => Config.MaxRetries;
+        set => Config.MaxRetries = value;
+    }
+    public int RetryDelay
+    {
+        get => Config.RetryDelay;
+        set => Config.RetryDelay = value;
+    }
+    public bool SkipFailedDownloads
+    {
+        get => Config.SkipFailedDownloads;
+        set => Config.SkipFailedDownloads = value;
+    }
+}
